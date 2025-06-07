b7b713815b9ede94b24b86288cc156834402545d
Kuriiios <cyril.leconte07@gmail.com>
2025-06-07 12:16:57 +0200

    corrected for deployement

--- deployement/app.py
+++ deployement/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from utils import make_prediction
+from .utils import make_prediction
 
 app = Flask(__name__)
 
@@ -12,14 +12,15 @@ def home():
 def predict():
     email_text = request.form.get('content')
     email_text = [email_text]
-    predictions = make_prediction(email_text)
-    return render_template('index.html', predictions=predictions, email_text=email_text)
+    prediction = make_prediction(email_text)
+    return render_template('index.html', prediction=prediction, email_text=email_text)
 
 @app.route('/api/predict', methods=['POST'])
 def api_predict():
     data = request.get_json(force = True)
     email_text = data['content']
-    predictions = make_prediction(email_text)
-    return jsonify({'predictions': predictions, 'email_text': email_text})  
+    prediction = make_prediction(email_text)
+    return jsonify({'prediction': prediction, 'email_text': email_text})  
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
