634f393c89bb0711130bc5703c834b8bb3f6a90f
Kuriiios <cyril.leconte07@gmail.com>
2025-06-07 13:38:07 +0200

    trying fix deployement

--- deployement/utils.py
+++ deployement/utils.py
@@ -6,7 +6,7 @@ model  = pickle.load(open("deployement/models/clf.pkl", 'rb'))
 def make_prediction(email_text):
     if email_text == "":
         return ""
-    tokenized_email = tokenizer.transform(email_text)
+    tokenized_email = tokenizer.transform([email_text])
     prediction = model.predict(tokenized_email)
     prediction = 1 if prediction == 1 else -1
     return prediction
